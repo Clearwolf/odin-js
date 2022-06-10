0437c9520a087572a22d68215611ba5eda7b8680
Clearwolf <dev@techromero.com>
2022-06-10 13:50:06 -0400

    Add all if/else statements to function play round

--- myscipts.js
+++ myscipts.js
@@ -1,12 +1,40 @@
 function computerPlay() {
+
     const playOptions = ["Rock", "Paper", "Scissors"];
+
     const randomPlay = playOptions[Math.floor(Math.random() * playOptions.length)];
+
     return randomPlay;
+
 }
 
 function playSingleRound(playerSelection, computerSelection) {
+
+    const win = "You win. You beat the computer. You are the superior being.";
+    const lose = "You lose. Now would be a good time to reflect on the decisions you've made in your life that have led you to this moment.";
+    const tie = "You tied. Nobody wins, everyone gets a participation trophy."
+
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return "You tied.  Nobody wins, everyone gets a participation trophy."};
+        return tie}
+
+    else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
+        return lose}
+
+    else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+        return win}
+
+    else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        return win}
+
+    else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
+        return lose}
+        
+    else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
+        return lose}
+
+    else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        return win}
+
 }
 
 const playerSelection = "rock";
