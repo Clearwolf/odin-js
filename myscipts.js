a3d2a81bf0ce43c5ffb5e0066a095111a8960b54
Clearwolf <dev@techromero.com>
2022-06-10 13:18:48 -0400

    Bug fix for const and playround function
    Moved consts outside of function for playsinglround

--- myscipts.js
+++ myscipts.js
@@ -4,11 +4,12 @@ function computerPlay() {
     return randomPlay;
 }
 
-function playSingleRound() {
-    const playerSelection = "rock";
-    const computerSelection = computerPlay();
+function playSingleRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         return "You tied.  Nobody wins, everyone gets a participation trophy."};
 }
 
-    console.log(playSingleRound());
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playSingleRound(playerSelection, computerSelection));
