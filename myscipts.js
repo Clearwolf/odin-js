c2fc308839df49bae678e228f601f057c355b3d7
Clearwolf <dev@techromero.com>
2022-06-10 12:31:30 -0400

    Added function for selecting radom computer play

--- myscipts.js
+++ myscipts.js
@@ -1,2 +1,7 @@
-let x = 20;
-console.log(x)
+function computerPlay() {
+    const playOptions = ["Rock", "Paper", "Scissors"];
+    const randomPlay = playOptions[Math.floor(Math.random() * playOptions.length)];
+    return randomPlay;
+}
+
+console.log(computerPlay());
